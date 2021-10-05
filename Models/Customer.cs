37afdf572b9d7983342da9c0ef82032e4f2152a8
simona pop <pop.simona.ps@gmail.com>
2021-10-05 20:11:34 +0300

    Add project files.

---
+++ Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Pop_Simona_Lab2.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)] //cheia primara e introdusa
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public DateTime BirthDate { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
